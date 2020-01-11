538c6f159155c60da918797e9bd12a2b7fd4f4e8
Carlos <cpinelly@gmail.com>
2020-01-11 17:57:08 -0500

    :tada:

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import './screens/products_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shop App',
+      theme: ThemeData(
+
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange
+      ),
+      home: ProductsOverviewScreen(),
+    );
+  }
+
+}
+
+
