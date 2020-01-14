cc9cd13e4cb55c431a8ecd828c5d2d0b6d5f10fb
Carlos <cpinelly@gmail.com>
2020-01-14 09:25:23 -0500

    Created orders provider.

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import './screens/product_details_screen.dart';
 import './screens/cart_screen.dart';
 
 import './providers/products.dart';
+import './providers/orders.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,6 +21,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider.value(
           value: Cart(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Orders(),
         )
       ],
       child: MaterialApp(
