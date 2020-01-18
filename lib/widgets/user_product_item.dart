29fd90dc2e036d59e3a45925f495dce6edc95c94
Carlos <cpinelly@gmail.com>
2020-01-18 16:47:20 -0500

    Deleting products.

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 import '../screens/edit_product_screen.dart';
 
@@ -29,7 +32,9 @@ class UserProductItem extends StatelessWidget {
             ),
             IconButton(
               icon: Icon(Icons.delete),
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
               color: Theme.of(context).errorColor,
             )
           ],
