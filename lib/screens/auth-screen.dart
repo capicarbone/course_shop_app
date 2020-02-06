ae6cff52aec7ec9ae8926c2a3033b65045da8d5c
Carlos <cpinelly@gmail.com>
2020-02-06 09:22:43 -0500

    Animating auth screen by the long way.

--- lib/screens/auth-screen.dart
+++ lib/screens/auth-screen.dart
@@ -93,7 +93,8 @@ class AuthCard extends StatefulWidget {
   _AuthCardState createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.Login;
   Map<String, String> _authData = {
@@ -102,19 +103,50 @@ class _AuthCardState extends State<AuthCard> {
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
+  AnimationController _animController;
+  Animation<Size> _heightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+
+    _heightAnimation = Tween<Size>(
+      begin: Size(double.infinity, 260),
+      end: Size(double.infinity, 320),
+    ).animate(
+      CurvedAnimation(
+        parent: _animController,
+        curve: Curves.linear,
+      ),
+    );
+
+    _heightAnimation.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _animController.dispose();
+    super.dispose();
+  }
 
   void _showErrorDialog(String message) {
-    showDialog(context: context, builder: (ctx) => AlertDialog(
-      title: Text('An Error Ocurred!'),
-      actions: <Widget>[
-        FlatButton(
-          child: Text('Okay'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          } ,
-        )
-      ],
-    ));
+    showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+              title: Text('An Error Ocurred!'),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('Okay'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ));
   }
 
   Future<void> _submit() async {
@@ -153,9 +185,9 @@ class _AuthCardState extends State<AuthCard> {
       }
 
       _showErrorDialog(errorMessage);
-
     } catch (error) {
-      const errorMessage = 'Could not authentication you. Please try again later.';
+      const errorMessage =
+          'Could not authentication you. Please try again later.';
 
       _showErrorDialog(errorMessage);
     }
@@ -170,10 +202,12 @@ class _AuthCardState extends State<AuthCard> {
       setState(() {
         _authMode = AuthMode.Signup;
       });
+      _animController.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
+      _animController.reverse();
     }
   }
 
@@ -186,9 +220,9 @@ class _AuthCardState extends State<AuthCard> {
       ),
       elevation: 8.0,
       child: Container(
-        height: _authMode == AuthMode.Signup ? 320 : 260,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        //height: _authMode == AuthMode.Signup ? 320 : 260,
+        height: _heightAnimation.value.height,
+        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
         width: deviceSize.width * 0.75,
         padding: EdgeInsets.all(16.0),
         child: Form(
